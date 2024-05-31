ec4d3d149a7737945b8c10c4e6470e4841833ed0
trdmonkey <trdmonkey218@gmail.com>
2024-05-30 22:12:14 -0500

    creando proyecto

---
+++ config/config.php
@@ -0,0 +1,23 @@
+<?php
+
+// HOST
+define("HOST", "localhost");
+
+// DBBNAME
+define("DBNAME", "ecommerce");
+
+// USER
+define("USER", "adsi");
+
+// PASS
+define("PASS", "utilizar");
+
+$conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME.";user=".USER.";password=".PASS); 
+
+
+
+
+
+
+
+
